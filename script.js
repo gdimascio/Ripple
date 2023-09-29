a33d1ec34a860f80a7bc79847ab6441709250d26
gdimascio <guido.dimascio@gmail.com>
2023-09-29 14:05:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// var poolBg = document.querySelector(".pool_bg");
+// console.log(poolBg)
+
+// poolBg.addEventListener('mousemove', (event) => {
+//     console.log("enter img")
+//     const ripple = document.createElement("span");
+//     ripple.classList.add("ripple");
+
+//     // Position ripple
+//     const x = event.clientX - poolBg.offsetLeft;
+//     const y = event.clientY - poolBg.offsetTop;
+//     ripple.style.left = x + "px";
+//     ripple.style.top = y + "px";
+
+//     poolBg.appendChild(ripple);
+
+//   // Animate and remove ripple
+//     setTimeout(() => {
+//         ripple.remove();
+//     }, 1000);
+// });
+
+
